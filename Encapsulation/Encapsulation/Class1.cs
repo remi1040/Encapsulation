68d8a9d77dfa9ad6fffd83a88fb22ef3da4ce215
remi1040 <48013030+remi1040@users.noreply.github.com>
2024-06-17 16:55:58 +0200

    Add files via upload

---
+++ Encapsulation/Encapsulation/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class BeerEncapsulator
+    {
+        private float _avalaibleBeerVolume;
+        private int _avalaibleBottles;
+        private int _avalaibleCapsules;
+
+        public BeerEncapsulator(float volumeBeer, int nombreBouteilles, int nombreCapsule) 
+        {
+            this._avalaibleBeerVolume = volumeBeer;
+            this._avalaibleBottles = nombreBouteilles;
+            this._avalaibleCapsules = nombreCapsule;
+        }
+
+        public void AddBeer(float ajoutBiere)
+        {
+            this._avalaibleBeerVolume += ajoutBiere;
+        }
+
+        public int ProduceEncapsulatedBeerBottles(int nombreBouteilles)
+        {
+            if (nombreBouteilles <= this._avalaibleBottles && nombreBouteilles <= this._avalaibleCapsules && nombreBouteilles*0.33 <= this._avalaibleBeerVolume) 
+            {
+                int nombreBouteillesRempli = 0;
+
+                for ( int i = 0; i < nombreBouteilles; i++)
+                {
+                    if (_avalaibleBeerVolume > 0.33)
+                    {
+                        this._avalaibleBottles -= 1;
+                        this._avalaibleCapsules -= 1;
+                        this._avalaibleBeerVolume -= 0.33f;
+                        nombreBouteillesRempli += 1;
+                    }
+                }
+                return nombreBouteillesRempli;
+            }
+            else
+            {
+                if (nombreBouteilles > this._avalaibleBottles)
+                {
+                    Console.WriteLine($"Il va manquer {nombreBouteilles - this._avalaibleBottles} bouteilles ! ");
+                }
+                if (nombreBouteilles > this._avalaibleCapsules)
+                {
+                    Console.WriteLine($"Il va manquer {nombreBouteilles - this._avalaibleCapsules} capsule ! ");
+                }
+                if (nombreBouteilles * 0.33 > this._avalaibleBeerVolume)
+                {
+                    Console.WriteLine($"Il va manquer {this._avalaibleBeerVolume - nombreBouteilles * 0.33 } cl de bieres ! ");
+                }
+                return 0;
+            }
+        }
+
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using Encapsulation;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        float number1;
+        int number2;
+        int number3;
+
+        while (true)
+        {
+            Console.WriteLine("Entrez une quatité de biere en litres: ");
+            float.TryParse(Console.ReadLine(), out number1);
+            Console.WriteLine("Entrez un nombre de bouteilles de biere disponible : ");
+            int.TryParse(Console.ReadLine(), out number2);
+            Console.WriteLine("Entrez un nombre de capsule de biere disponible : ");
+            int.TryParse(Console.ReadLine(), out number3);
+
+            if (number1 > 0.33f && number2 > 0 && number3 > 0)
+            {
+                break;
+            }
+
+            
+        }
+
+        BeerEncapsulator b1 = new BeerEncapsulator(number1, number2, number3);
+        Console.WriteLine(b1.ProduceEncapsulatedBeerBottles(5));
+
+    }
+}
